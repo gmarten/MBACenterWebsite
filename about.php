3c050476b016b87793f43f45bdb24eae6ea2a3e3
Günther <gunther@mbacentereurope.eu>
2015-03-30 01:59:36 +0200

    Folder cleanup & About page
    -lightbox and fancy box folders deleted
    -about page & about.css file added
    -path reference in GMAT style.css corrected

---
+++ about.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Theme Starz">
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/about.css" type="text/css">
+    <link rel="stylesheet" href="at/gmat/css/bootstrap/bootstrap-responsive.css" type="text/css">
+    <link rel="stylesheet" href="at/gmat/css/animations/animate.css" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300" rel='stylesheet' type='text/css'>
+
+    <title>MBA Center - About Us</title>
+
+</head>
+
+<body class="page-sub-page page-about-us">
+    <!-- Wrapper -->
+    <div class="wrapper">
+
+
+        <!-- Header -->
+        <?php include('header.php'); ?>
+        <!-- end Header -->
+
+        <!-- Breadcrumb -->
+        <div class="container">
+            <ol class="breadcrumb">
+                <li><a href="/">Home</a></li>
+                <li class="active">About Us</li>
+            </ol>
+        </div>
+        <!-- end Breadcrumb -->
+
+        <!-- Welcome image -->
+        <section class="container info">
+                <img src="assets/img/about_welcome.png" alt="Classroom" />
+        </section>
+        <!-- End welcome image -->
+
+        <!-- Page Content -->
+        <section class="container">
+            <div class="row-fluid">
+
+                <div class="span4">
+                    <h3 class="animated delay1 fadeInDown">MBA Center Brussels</h3>
+                    <p class="animated delay1 fadeInUp">The MBA Center Brussels is one of the oldest MBA Centers in the world; it was created in 1998 and has trained thousands of students to the TOEFL, IELTS, SAT and GMAT.</p>
+                    <p class="animated delay1 fadeInUp">The MBA Center Brussels is a gateway to top European and US business schools, with hundreds of students already accepted at top universities and business schools. The MBA Center Brussels is known for its outstanding success rate in MBAs, EMBAs and MIMs with students accepted at Harvard, NYU, INSEAD, LBS, IE, IESE, HEC, ESCP Europe…</p>
+                    <p class="animated delay1 fadeInUp">In order to get such academic great results, the MBA Center has developed a unique expertise to allow its students score high on standardized tests such as the GMAT, SAT, TOEFL, TOEIC, and IELTS. The MBA Center offers courses adapted to each your needs: group classes, online classes and private tutoring.</p>
+                    <p class="animated delay1 fadeInUp">Thanks to its reputation, MBA Center is also the official partner of Educational Testing Services (TOEFL, SAT, GRE…) and the exclusive sponsor of the QS World MBA tour.</p>
+                </div>
+
+                <div class="span4">
+                    <div class="top-item">
+                        <h3 class="iconset iconbestcity animated delay2 fadeInDown">Study with test experts</h3>
+                        <p class="animated delay2 fadeInUp">MBA Center Brussels only employs teachers who score 740+ on the GMAT and who participate in its R&D program. They are all experts in the test they teach.  The books produced by MBA Center are recognized for their quality and published by the largest educational publishers in Europe (Hobsons, Studyrama, Hachette Livres, Ellipses…).</p>
+                        <p class="animated delay2 fadeInUp"> Finally the MBA Center courses are recommended by ESSEC (France), EBS (Germany) and Politecnico di Milano (Italy), all of whom use our test-prep services.</p>
+                    </div>
+
+                    <div class="bottom-item">
+                        <h3 class="iconset iconworldwide animated delay2 fadeInDown">Unlimited  Number of Teaching Hours</h3>
+                        <p class="animated delay2 fadeInUp">Our intensive course lasts 40 hours; you can renew as many times as you want. In addition we correct a different test each week throughout the year (Destination 800).</p>
+                    </div>
+                </div>
+
+                <div class="span4">
+                    <div class="top-item">
+                        <h3 class="iconset iconcommunity animated delay3 fadeInDown">Material developed by our own experts</h3>
+                        <p class="animated delay3 fadeInUp">MBA Center is the premier test prep center placed in non-English speaking countries,
+                            with students who are aiming for top business schools; therefore, MBA Center has developed a strong methodology to help non-speaking students to score high on the Verbal.
+                        </p>
+                    </div>
+                    <div class="bottom-item">
+                        <h3 class="iconset iconfacilities animated delay3 fadeInDown">Comprehensive Service</h3>
+                        <p class="animated delay3 fadeInUp">The GMAT is only one from a selection of many factors from which business schools will select candidates, the others being international exposure, leadership, grades, professional qualifications, quality of the professional project, the contribution of the student to the school's community, and finally diversity. </p>
+                        <p class="animated delay3 fadeInUp">MBA Center has the premier coaching department where we will train you to convey in your CV, your essays, letter of reference, interview, and the requirements of the schools. </p>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+
+        <!--Google Maps-->
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.2366156238745!2d4.400245000000008!3d50.82678100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c4b5c2317f55%3A0x164fa5e119f81e8!2sICAB+Incubator!5e0!3m2!1sfr!2sfr!4v1421241801167" width="100%" height="350" scrolling="no" frameborder="0" style="border:2px; overflow:hidden;"></iframe>
+        <!--End Google Maps-->
+
+        <!-- end Page Content -->
+
+        <!-- Footer -->
+        <?php include('footer.php'); ?>
+        <!-- end Footer -->
+
+    </div>
+    <!-- end Wrapper -->
+
+    <script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/selectize.min.js"></script>
+    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+    <script type="text/javascript" src="assets/js/jQuery.equalHeights.js"></script>
+    <script type="text/javascript" src="assets/js/icheck.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.vanillabox-0.1.5.min.js"></script>
+    <script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -728,72 +728,7 @@ type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 <!-- end Page Content -->
 
 <!-- Footer -->
-<footer id="page-footer">
-    <section id="footer-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-12">
-                    <aside class="logo">
-                        <img src="assets/img/logo-white.png" class="vertical-center">
-                    </aside>
-                </div><!-- /.col-md-3 -->
-                <div class="col-md-3 col-sm-4">
-                    <aside>
-                        <header><h4>Contact Us</h4></header>
-                        <address>
-                            <strong>MBA Center Brussels</strong>
-                            <br>
-							<span>ICAB/Site Arsenal</span>
-                            <br>
-                            <span>4 Rue des Pères Blancs</span>
-                            <br>
-							<span>Etterbeck</span>
-                            <br><br>
-                            <span>B-1040 Brussels </span>
-                            <br>
-                            <abbr title="Telephone">Telephone:</abbr> +32 (0)2 737 65 05
-                            <br>
-                            <abbr title="Email">Email:</abbr> <a href="#">hubert@mbacentereurope.eu</a>
-                        </address>
-                    </aside>
-                </div><!-- /.col-md-3 -->
-                <div class="col-md-3 col-sm-4">
-                    <aside>
-                        <header><h4>Courses and services</h4></header>
-                        <ul class="list-links">
-                            <li><a href="#">GMAT</a></li>
-                            <li><a href="#">B-School admission</a></li>
-                            <li><a href="#">consulting</a></li>
-                            <li><a href="#">SAT</a></li>
-                            <li><a href="#">TOEFL</a></li>
-                            <li><a href="#">IELTS</a></li>
-                            <li><a href="#">Solvay test</a></li>
-                        </ul>
-                    </aside>
-                </div><!-- /.col-md-3 -->
-                <div class="col-md-3 col-sm-4">
-                    <aside>
-					<br><br><br><br>
-					<header><h4>DISCOUNT</h4></header>
-                        <p>The MBA Center offers monthly a 10% discount to the first ten students that register for one of our programs (ex: SAT, TOEFL, etc.). In order to
-                        benefit from this discount contact us with the discount code: BRU4.</p>                    
-                        
-                    </aside>
-                </div><!-- /.col-md-3 -->
-            </div><!-- /.row -->
-        </div><!-- /.container -->
-        <div class="background"><img src="assets/img/background-city.png" class="" alt=""></div>
-    </section><!-- /#footer-content -->
-
-    <section id="footer-bottom">
-        <div class="container">
-            <div class="footer-inner">
-                <div class="copyright">© <a href="http://www.clementh.com" target="_blank">Clément Hadjeres</a>, All rights reserved</div><!-- /.copyright -->
-            </div><!-- /.footer-inner -->
-        </div><!-- /.container -->
-    </section><!-- /#footer-bottom -->
-
-</footer>
+    <?php include('footer.php'); ?>
 <!-- end Footer -->
 
 
